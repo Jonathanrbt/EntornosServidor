487abadc444a36c0c2b3f4422b829698faa91aa5
Jonathan Romero <jonathanstiwarromerobetancourt@gmail.com>
2024-06-03 10:46:02 -0500

    Mensaje de confirmación

---
+++ tienda-api-spring-boot/src/test/java/dev/joseluisgs/tiendaapispringboot/TiendaApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joseluisgs.tiendaapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
